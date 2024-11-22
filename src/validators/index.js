e2a62f154e8e2b9a0aa8cfde6cdedefcbc288359
Karim-Muhammad <kimoomar007@gmail.com>
2024-11-22 16:31:34 +0200

    new format for error validation

--- src/validators/index.js
+++ src/validators/index.js
@@ -3,9 +3,10 @@ const ApiError = require("../utils/ApiError");
 
 exports.doValidate = (req, res, next) => {
   console.log("~~~~~~~~~~~~~~~~~~~~ doValidate ~~~~~~~~~~~~~~~~~~~~");
-  const errors = validationResult(req).formatWith(({ msg, param }) => ({
+  const errors = validationResult(req).formatWith(({ msg, param, path }) => ({
     field: param,
     message: msg,
+    [path]: msg,
   }));
 
   console.log(req.body);
